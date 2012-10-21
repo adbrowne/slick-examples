309003ce988eeb2e75a3dccf14306763d3745918
= <=>
2012-10-21 18:19:45 +1100

    Split Macros to their own file

--- src/main/scala/scala/slick/examples/rough/DirectRough.scala
+++ src/main/scala/scala/slick/examples/rough/DirectRough.scala
@@ -1,86 +1,27 @@
-package scala.slick.example.presentation
-import scala.language.experimental.macros // turns on the macros
-import scala.reflect.runtime.{ universe => ru }
-import scala.reflect.ClassTag
-import scala.reflect.macros.Context
+package scala.slick.examples.rough
+
+import scala.slick.examples.rough._
 
 object DirectRough extends App {
 
-  class Session
-  class Database {
-    def withSession[T](f: Session => T): T = {
-      val s = new Session
-      f(s)
-    }
-  }
 
-  object Database {
-    val session = new Session
-
-    implicit def threadLocalSession: Session = {
-      session
-    }
-    def forURL(connectionString: String, driver: String): Database = {
-      new Database
-    }
-  }
-  object QueryableMacros {
-    def filter[T: c.AbsTypeTag]
-      (c: scala.reflect.macros.Context)
-      (projection: c.Expr[T => Boolean]): c.Expr[Queryable[T]] = {
-      import c.universe._
-      c.universe.reify(new Queryable[T])
-    }
-    
-    def map[T:c.AbsTypeTag, S:c.AbsTypeTag]
-      (c: scala.reflect.macros.Context)
-      (projection: c.Expr[T => S]): c.Expr[Queryable[S]] = {
-      c.universe.reify(new Queryable[S])
-    }
-  
-  }
-
-  class Queryable[T] {
-    def filter(projection: T => Boolean): Queryable[T] = macro QueryableMacros.filter[T]
-    def withFilter(projection: T => Boolean): Queryable[T] = macro QueryableMacros.filter[T]
-    def map[S]( projection: T => S ) : Queryable[S] = macro QueryableMacros.map[T,S]
-  }
-
-  class SlickBackend
-
-  object Queryable {
-    def apply[T: ru.TypeTag: ClassTag]() = new Queryable[T]()
-  }
-
-  class ImplicitQueryable[T](q: Queryable[T], backend: SlickBackend, session: Session) extends Queryable {
-
-  }
-
-  object ImplicitQueryable {
-    def apply[T](q: Queryable[T], backend: SlickBackend)(implicit session: Session) = new ImplicitQueryable[T](q, backend, session)
-
-  }
 
   case class TrainTrip(
     name: String,
     price: Double,
     countryID: Int)
 
-  Database.forURL("jdbc:h2:mem:test1;TRACE_LEVEL_FILE=4", driver = "org.h2.Driver") withSession {
-    import Database.threadLocalSession
+ 
+    val trips = Queryable[TrainTrip]
 
-    scala.slick.examples.presentation.Helper.createDb
-
-    val backend = new SlickBackend()
-
-    val trips = ImplicitQueryable(Queryable[TrainTrip], backend)
-
-    val australianTrips = for {
-      t <- trips // if t.countryID == 1
+ val australianTrips = for {
+      t <- trips if t.countryID == 1
     } yield (t.name, t.price)
 
-    australianTrips.foreach {
-      case (name, price) => println(name + ": " + price)
+//    val australianTrips : Queryable[TrainTrip] = trips
+    
+    australianTrips.toSeq.foreach {
+      case trip => println(trip.name)
     }
-  }
+ 
 }
---
+++ src/main/scala/scala/slick/examples/rough/StubbedMacros.scala
@@ -0,0 +1,33 @@
+package scala.slick.examples.rough
+
+import scala.language.experimental.macros // turns on the macros
+import scala.reflect.runtime.{ universe => ru }
+import scala.reflect.ClassTag
+import scala.reflect.macros.Context
+
+  object QueryableMacros {
+    def filter[T: c.AbsTypeTag]
+      (c: scala.reflect.macros.Context)
+      (projection: c.Expr[T => Boolean]): c.Expr[Queryable[T]] = {
+      import c.universe._
+      c.universe.reify(new Queryable[T])
+    }
+    
+    def map[T:c.AbsTypeTag, S:c.AbsTypeTag]
+      (c: scala.reflect.macros.Context)
+      (projection: c.Expr[T => S]): c.Expr[Queryable[S]] = {
+      c.universe.reify(new Queryable[S])
+    }
+  
+  }
+
+  class Queryable[T] {
+	def toSeq : Seq[T] = List()
+    def filter(projection: T => Boolean): Queryable[T] = macro QueryableMacros.filter[T]
+    def withFilter(projection: T => Boolean): Queryable[T] = macro QueryableMacros.filter[T]
+    def map[S]( projection: T => S ) : Queryable[S] = macro QueryableMacros.map[T,S]
+  }
+
+  object Queryable {
+    def apply[T: ru.TypeTag: ClassTag]() = new Queryable[T]()
+  }
